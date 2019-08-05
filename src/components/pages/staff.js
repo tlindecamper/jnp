b4c25582d028f04a6c05a097516564b57d1a4efc
tlindecamper <therelan2001@yahoo.com>
2019-08-05 13:54:26 -0600

    added staff page

--- src/components/pages/staff.js
+++ src/components/pages/staff.js
@@ -1,12 +1,157 @@
 import React, { Component } from 'react';
 
+
 export default class Staff extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Our Staff </h1>
+      <div className='staff-wrapper'>
+        <h1>Meet Our Staff </h1>
+        <div className='staff-container'>
+          <div className='staff-columns'>
+            <div className='staff-column'>
+              <div className='staff-pic'>
+                <img src='/assets/staff-pics/Jessica+BWB.jpg'></img>
+              </div>
+              <h1> +Jessica</h1>
+              <div className='staff-bio'>
+                <h2>
+                Jessica started her professional career dancing for companies with Mia Micheals, Dee Caspary, and Odyssey Dance Theater. She then traveled to Japan to do several
+                contracts with Tokyo Disney in Broadway production 
+                "Encore" and outdoor theater "Showbiz is!" Where she was also named Dance Captain . From there she was then honored to be selected as Assistant 
+                director in the Radio City Music Hall Christmas Spectacular with broadway legend Linda Haberman for 3 years. After finishing there, she had the 
+                greatest honor of joining Celine Dion in "A New Day" in the colosseum Ceasers palace Las Vegas where she was chosen to work side by side with the 
+                renowned choreographer Mia Micheals in the reVamp of the show. 
+                She then continued with Franco Dragones water spectacular "Le Reve" at the Wynn. She background sang and danced for Vegas staple Dian Diaz ,
+                performed with Elton John and Jerry Sienfield in a Katrina tribute and Completed her Vegas stay at Cirque du Soliel's "Viva Elvis". Jessica Loves
+                  Teaching, and has been sharing this passion throughout her entire Dance Career.
+                She is experienced in many styles including Contemporary, jazz, lyrical, ballroom, hip-hop all extending from an extensive ballet background. 
+                Jessica is also a 500hr Yoga Alliance Certified instructor and had the oppurtunity to lead teacher trainings and act as Director at a local studio. She is excited to be teaching at Jazz’n Place again!!!
+                </h2>
+              </div>
+              </div>
+            <div className='staff-column'>
+              <div className='staff-pic'>
+                <img src='/assets/staff-pics/Jarica+BWB.jpg'></img>
+              </div>
+              <h1> +Jarica</h1>
+              <div className='staff-bio'>
+                <h2>
+                Jarica is currently a 4th year student at The University of Utah studying Marketing & Advertising. Jarica grew up training at The Jazz’n Place Dance Studio under many qualified 
+                teachers such as Alan Salazar, Julia Miller Bryce, Angela Matson, Allison Debona and Jason Linsley. She is trained in contemporary, hip-hop, ballet and jazz. She now spec
+                choreographing in hip-hop and contemporary styles. She has been awarded hip-hop CREW from Adrenaline Dance Convention in 2012 and 2013 and was the MVA runner up at Velocity
+                Dance Convention in 2013. From 2013-2015 she was a part of San Diego State University’s hip-hop team, K.O, where she performed at many campus events as well as around the 
+                San Diego area. Jarica is passionate about teaching and has been doing so since she was 13 years old. Teaching is anything but a job to her and she hopes to continue to grow
+                  and influence the lives of many dancers for a very long time.
+                </h2>
+              </div>
+            </div>
+
+            
+            <div className='staff-column'>
+              <div className='staff-pic'>
+                <img src='/assets/staff-pics/Natalie+BWB.jpg'></img>
+              </div>
+              <h1> +Natalie</h1>
+              <div className='staff-bio'>
+                <h2>
+                Natalie began her training at the age of three at Southern California Ballet formerly known as Black Mountain Dance Centre in San Diego, California. Under the directorship 
+                of Sylvia Palmer-Zetler and Ahita Ardalen where she performed both soloist and principal roles with Southern California Ballet. She attended summer intensives on Merit Scholarship
+                at THE Royal Ballet School in London, England, Boston Ballet, Ballet Met and Cecchetti USA. Natalie was accepted into Julliard, the prestige institute of performing Arts in
+                New York City. She trained in the Cecchetti Method where she completed all Cecchetti exams with honors. Natalie then relocated to Utah and continued her ballet training at
+                the University of Utah at the highest level with Utah ballet where she continuing her education. Some of her lead roles include: Clara, Sugar Plum Fairy and Snow Queen in 
+                the Nutcracker, Fairy God Mother in Cinderella and the Bird in Peter and the Wolf. In 2011, she was a part of Gregory Dawson’s original piece “In the Midst of a Southern 
+                Storm” and in 2013 she had the pleasure of working with Jay Kim on his original piece “Enrapture” as a member of Utah Ballet. Some of her teachers include Suzan Stone,
+                Nancy Robinson, Maureen Laird, Jay Kim and Michael Bearden.
+                </h2>
+              </div> 
+          </div>
+          </div>        
+          
+      <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Staci+BWB.jpg'></img>
+            </div>
+            <h1> +Staci</h1>
+            <div className='staff-bio'>
+            <h2>
+            Staci was born and raised in Utah. She received her ballet training at BYU as a Theatre Ballet Youth Artist 
+            under the direction of Connie Burton and Janalyn Memmott. She also trained in jazz, tap, tumbling, and hip- hop 
+            at The Dance Club on their Jr. and Sr. Company. Staci received her Bachelor’s of Fine Arts and Master’s of Fine 
+            Arts on full scholarship from the University of Utah Department of Ballet with an emphasis in teaching, performing
+             and choreography. At the U of U she danced principal and soloist roles with Utah Ballet. Her favorite roles include
+              The Snow Maiden in The Snow Maiden, Titania in A Mid Summer Night’s Dream and the Pas de Deux Soloist in George 
+              Balanchine’s Concerto Barocco. Staci has been dancing professionally for the past ten years. She danced with Odyssey 
+              in 2005. She then left Utah to play Wendy and Sleeping Beauty in Japan at the Tokyo Disneyland Resort. She was the pointe
+               soloist on Holland America’s MS Zuiderdam and MS Veendam where she sailed the Mediterranean Sea and the Caribbean. 
+               She has performed with the Radio City Christmas Spectacular in several different tours as a featured ensemble member
+                and in 2011 Staci had the privilege of performing the Radio City Christmas Spectacular in NYC at the iconic
+                 Radio City Music Hall. Last summer she performed at the Hale Center Theater in West Valley in 9 to 5 The Musical. 
+                 She has also performed in several industrials throughout the US, Australia and Japan. Staci has been teaching
+                  and choreographing jazz and ballet at privately owned studios since she was 18. She has taught and choreographed
+                   for several studios throughout the US and Australia. She has taught undergraduate classes at the University of
+                    Utah. A piece she choreographed for Utah Ballet was performed at the Kennedy Center in Washington DC for the 
+                    ACDFA Gala. Staci is currently on faculty at UVU teaching ballet and jazz.
+            </h2>
+            </div>
+          </div>
+          <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Mysti+BWB.jpg'></img>
+            </div>
+            <h1> +Mysti</h1>
+            <div className='staff-bio'>
+            <h2>
+            Mysti grew up performing and competing numerous group numbers and solos. She started assisting and teaching at 
+            the studio at the age of 12; from there she has taught and choreographed for many teams and studios, and assisted 
+            for many local conventions. In high school, Mysti was on the school dance company as well as the drill team. She 
+            attended the Pulse Protégé Convention in New York on scholarship and also received many other scholarships 
+            from conventions around the country. After high school, she continued training and performing on Bonnie Story’s 
+            pre-professional company, Expansion. She then was a part of Krissie Edwards’ company, Divinity. She’s had the opportunity
+             to dance back-up for David Archuleta, be in NBA half-time performances, and be a featured dancer for a Miche Bag Fashion 
+             Show. Mysti then moved to Los Angeles to further pursue her dance career. She performed in Carnival under 
+             the direction of Chris Dupre, and again for Sam Allen. She’s had the opportunity to be a part of multiple
+              movies and television shows including High School Musical 3, American Mall, Dexter, Dancing Fools, pilots 
+              for future TV shows, and more. Throughout her life, she has taken classes and worked with some of the 
+              top choreographers and dancers in the entertainment industry. She had the privilege of being a part of 
+              Justin Giles’ SoulEscape professional dance company where she was able to travel to perform shows as
+               well as teach students as part of an intensive. Mysti was able to work with Ricky Palomino for his 
+               “Daylight Come” show, as well as Matt Cady for his Lana Del Rey/Disney submission and his very own 
+               show “It’s About Robots and Humans and the Meaning of Life”. She is now back in Utah teaching classes 
+               and choreography, and very much enjoys training and preparing students for their futures. She has
+                judged many competitions and auditions, as well as taught and choreographed for multiple local
+                 camps, conventions, drill teams, dance companies, studios, and more. She specializes in technique, 
+                 contemporary, lyrical, jazz, tumbling, and is a certified Acro Dance teacher from Acrobatic Arts.
+            </h2>
+            </div>
+          </div>
+          <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Kellie+BWB.jpg'></img>
+            </div>
+            <h1> +Kelli</h1>
+            <div className='staff-bio'>
+            <h2>
+            Kellie won the title of Miss Dance/Drill Team Utah. She was then invited to Nagoya Japan where she won
+             the title of Miss Dance/Drill team International and won top honors with the Minister of Foreign Affairs
+              Award. Kellie then continued her education at the University of Utah where she was the head Cheerleader. 
+              Kellie danced professionally with the Odyssey Dance Utah for seven years. She was the director of
+               Dance Utah Summer Camp. She had the opportunity to direct Odysseys Junior Company, O2, for two years. 
+               Kellie’s real passion is teaching and choreographing. She has been teaching for over 20 years. 
+               She was nominated for the Who’s Who among Top Teachers. She has choreographed for Odyssey Dance 
+               Theatre, Miss Utah, dancers in the Star Search, dancers on So You Think You Can Dance, and Dancing
+                with the Stars. Kellie choreographed a music video “All for Nothing” for Mark Ballas. Kellie has 
+                choreographed Peter Pam, Footloose, Rag Time, and Wizard of Oz at the Scera Shell Outdoor 
+                Theatre. Kellie was a co-founder of The Vibe Performing Arts. In 2011 the Vibe was finalist on 
+                Paula Abduls dance show Live to Dance. She has choreographed and directed shows including Move,
+                 One Night Only, and Evolution of Dance. She took her dancers to Hong Kong China for Chinese New Year Parade.
+            </h2>
+            </div>
+          </div>
         
       </div>
+    </div>
+
+            
     );
   }
 }
