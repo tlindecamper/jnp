6a5bf7257ff65c6e26ead92a11daf118ab83d9a3
tlindecamper <therelan2001@yahoo.com>
2019-08-06 14:31:58 -0600

    added news page

--- src/components/pages/news.js
+++ src/components/pages/news.js
@@ -3,11 +3,43 @@ import React, { Component } from 'react';
 export default class News extends Component {
   render() {
     return (
-      <div className='app'>
-        <div>
-          Testimonials
+      <div className='news-wrapper'>
+        <div className='header'>Studio News</div>
+        <div className='news-banner'>
+          <img className='banner-pic' src='/assets/news-pic/banner-edited.jpg' ></img>
         </div>
-        
+        <div className='news-column'>
+          <div className='column-pic'>
+            <img className='leaping' src='/assets/news-pic/bailey.jpg'></img>
+          </div>
+          <div className='words-wrapper'>
+            <div className='column-header'>THINGS TO REMEMBER</div>
+            <h2>
+            * All tuition and fees are due the 25th of each month. Please call the studio if you have any questions about your account. We are always here to help with questions or concerns. We will be emailing out a link shortly to create your account on Parent Portal to access your account more conveniently.
+            </h2>
+            <h2>
+            * Follow us on Facebook and Instagram for updates, studio information, and fun captured dance memories. Please keep an updated email with us so we can update information with you as well.  
+            </h2>
+
+          </div>
+        </div>
+
+        <div className='news-column'>
+          
+          <div className='words-wrapper'>
+            <div className='column-header'>STUDIO UPCOMING DATES</div>
+            <ul className='news-dates'>
+              <li>July 22nd-July 28th- Happy Pioneer Day!!!</li>
+              <li>July 29th - August 10th - Competing Team Summer Intensives</li>
+              <li> August 12-15th - JNP Summer Camps Start</li>
+              <li>September 3rd - Fall Dance Classes Begin</li>
+              
+            </ul>
+            </div>
+          <div className='column-pic'>
+            <img className='titus' src='/assets/news-pic/024_2441+EDIT.jpg'></img>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/pages/staff.js
+++ src/components/pages/staff.js
@@ -5,14 +5,14 @@ export default class Staff extends Component {
   render() {
     return (
       <div className='staff-wrapper'>
-        <h1>Meet Our Staff </h1>
+        <div className='header'>Meet Our Staff </div> 
         <div className='staff-container'>
           <div className='staff-columns'>
             <div className='staff-column'>
               <div className='staff-pic'>
                 <img src='/assets/staff-pics/Jessica+BWB.jpg'></img>
               </div>
-              <h1> +Jessica</h1>
+              <div className='header'>+ Jessica </div> 
               <div className='staff-bio'>
                 <h2>
                 Jessica started her professional career dancing for companies with Mia Micheals, Dee Caspary, and Odyssey Dance Theater. She then traveled to Japan to do several
@@ -33,7 +33,7 @@ export default class Staff extends Component {
               <div className='staff-pic'>
                 <img src='/assets/staff-pics/Jarica+BWB.jpg'></img>
               </div>
-              <h1> +Jarica</h1>
+              <div className='header'>+ Jarica </div> 
               <div className='staff-bio'>
                 <h2>
                 Jarica is currently a 4th year student at The University of Utah studying Marketing & Advertising. Jarica grew up training at The Jazz’n Place Dance Studio under many qualified 
@@ -51,7 +51,7 @@ export default class Staff extends Component {
               <div className='staff-pic'>
                 <img src='/assets/staff-pics/Natalie+BWB.jpg'></img>
               </div>
-              <h1> +Natalie</h1>
+              <div className='header'>+ Natalie </div> 
               <div className='staff-bio'>
                 <h2>
                 Natalie began her training at the age of three at Southern California Ballet formerly known as Black Mountain Dance Centre in San Diego, California. Under the directorship 
@@ -66,88 +66,142 @@ export default class Staff extends Component {
               </div> 
           </div>
           </div>        
-          
-      <div className='staff-column'>
+        <div className='staff-columns'>
+          <div className='staff-column'>
             <div className='staff-pic'>
               <img src='/assets/staff-pics/Staci+BWB.jpg'></img>
             </div>
-            <h1> +Staci</h1>
+            <div className='header'>+ Staci </div> 
             <div className='staff-bio'>
-            <h2>
-            Staci was born and raised in Utah. She received her ballet training at BYU as a Theatre Ballet Youth Artist 
-            under the direction of Connie Burton and Janalyn Memmott. She also trained in jazz, tap, tumbling, and hip- hop 
-            at The Dance Club on their Jr. and Sr. Company. Staci received her Bachelor’s of Fine Arts and Master’s of Fine 
-            Arts on full scholarship from the University of Utah Department of Ballet with an emphasis in teaching, performing
-             and choreography. At the U of U she danced principal and soloist roles with Utah Ballet. Her favorite roles include
+              <h2>
+              Staci was born and raised in Utah. She received her ballet training at BYU as a Theatre Ballet Youth Artist 
+              under the direction of Connie Burton and Janalyn Memmott. She also trained in jazz, tap, tumbling, and hip- hop 
+              at The Dance Club on their Jr. and Sr. Company. Staci received her Bachelor’s of Fine Arts and Master’s of Fine 
+              Arts on full scholarship from the University of Utah Department of Ballet with an emphasis in teaching, performing
+              and choreography. At the U of U she danced principal and soloist roles with Utah Ballet. Her favorite roles include
               The Snow Maiden in The Snow Maiden, Titania in A Mid Summer Night’s Dream and the Pas de Deux Soloist in George 
               Balanchine’s Concerto Barocco. Staci has been dancing professionally for the past ten years. She danced with Odyssey 
               in 2005. She then left Utah to play Wendy and Sleeping Beauty in Japan at the Tokyo Disneyland Resort. She was the pointe
-               soloist on Holland America’s MS Zuiderdam and MS Veendam where she sailed the Mediterranean Sea and the Caribbean. 
-               She has performed with the Radio City Christmas Spectacular in several different tours as a featured ensemble member
-                and in 2011 Staci had the privilege of performing the Radio City Christmas Spectacular in NYC at the iconic
-                 Radio City Music Hall. Last summer she performed at the Hale Center Theater in West Valley in 9 to 5 The Musical. 
-                 She has also performed in several industrials throughout the US, Australia and Japan. Staci has been teaching
-                  and choreographing jazz and ballet at privately owned studios since she was 18. She has taught and choreographed
-                   for several studios throughout the US and Australia. She has taught undergraduate classes at the University of
-                    Utah. A piece she choreographed for Utah Ballet was performed at the Kennedy Center in Washington DC for the 
-                    ACDFA Gala. Staci is currently on faculty at UVU teaching ballet and jazz.
-            </h2>
+              soloist on Holland America’s MS Zuiderdam and MS Veendam where she sailed the Mediterranean Sea and the Caribbean. 
+              She has performed with the Radio City Christmas Spectacular in several different tours as a featured ensemble member
+              and in 2011 Staci had the privilege of performing the Radio City Christmas Spectacular in NYC at the iconic
+              Radio City Music Hall. Last summer she performed at the Hale Center Theater in West Valley in 9 to 5 The Musical. 
+              She has also performed in several industrials throughout the US, Australia and Japan. Staci has been teaching
+              and choreographing jazz and ballet at privately owned studios since she was 18. She has taught and choreographed
+              for several studios throughout the US and Australia. She has taught undergraduate classes at the University of
+              Utah. A piece she choreographed for Utah Ballet was performed at the Kennedy Center in Washington DC for the 
+              ACDFA Gala. Staci is currently on faculty at UVU teaching ballet and jazz.
+              </h2>
             </div>
           </div>
           <div className='staff-column'>
             <div className='staff-pic'>
               <img src='/assets/staff-pics/Mysti+BWB.jpg'></img>
             </div>
-            <h1> +Mysti</h1>
+            <div className='header'>+ Mysti </div> 
             <div className='staff-bio'>
-            <h2>
-            Mysti grew up performing and competing numerous group numbers and solos. She started assisting and teaching at 
-            the studio at the age of 12; from there she has taught and choreographed for many teams and studios, and assisted 
-            for many local conventions. In high school, Mysti was on the school dance company as well as the drill team. She 
-            attended the Pulse Protégé Convention in New York on scholarship and also received many other scholarships 
-            from conventions around the country. After high school, she continued training and performing on Bonnie Story’s 
-            pre-professional company, Expansion. She then was a part of Krissie Edwards’ company, Divinity. She’s had the opportunity
-             to dance back-up for David Archuleta, be in NBA half-time performances, and be a featured dancer for a Miche Bag Fashion 
-             Show. Mysti then moved to Los Angeles to further pursue her dance career. She performed in Carnival under 
-             the direction of Chris Dupre, and again for Sam Allen. She’s had the opportunity to be a part of multiple
+              <h2>
+              Mysti grew up performing and competing numerous group numbers and solos. She started assisting and teaching at 
+              the studio at the age of 12; from there she has taught and choreographed for many teams and studios, and assisted 
+              for many local conventions. In high school, Mysti was on the school dance company as well as the drill team. She 
+              attended the Pulse Protégé Convention in New York on scholarship and also received many other scholarships 
+              from conventions around the country. After high school, she continued training and performing on Bonnie Story’s 
+              pre-professional company, Expansion. She then was a part of Krissie Edwards’ company, Divinity. She’s had the opportunity
+              to dance back-up for David Archuleta, be in NBA half-time performances, and be a featured dancer for a Miche Bag Fashion 
+              Show. Mysti then moved to Los Angeles to further pursue her dance career. She performed in Carnival under 
+              the direction of Chris Dupre, and again for Sam Allen. She’s had the opportunity to be a part of multiple
               movies and television shows including High School Musical 3, American Mall, Dexter, Dancing Fools, pilots 
               for future TV shows, and more. Throughout her life, she has taken classes and worked with some of the 
               top choreographers and dancers in the entertainment industry. She had the privilege of being a part of 
               Justin Giles’ SoulEscape professional dance company where she was able to travel to perform shows as
-               well as teach students as part of an intensive. Mysti was able to work with Ricky Palomino for his 
-               “Daylight Come” show, as well as Matt Cady for his Lana Del Rey/Disney submission and his very own 
-               show “It’s About Robots and Humans and the Meaning of Life”. She is now back in Utah teaching classes 
-               and choreography, and very much enjoys training and preparing students for their futures. She has
-                judged many competitions and auditions, as well as taught and choreographed for multiple local
-                 camps, conventions, drill teams, dance companies, studios, and more. She specializes in technique, 
-                 contemporary, lyrical, jazz, tumbling, and is a certified Acro Dance teacher from Acrobatic Arts.
-            </h2>
+              well as teach students as part of an intensive. Mysti was able to work with Ricky Palomino for his 
+              “Daylight Come” show, as well as Matt Cady for his Lana Del Rey/Disney submission and his very own 
+              show “It’s About Robots and Humans and the Meaning of Life”. She is now back in Utah teaching classes 
+              and choreography, and very much enjoys training and preparing students for their futures. She has
+              judged many competitions and auditions, as well as taught and choreographed for multiple local
+              camps, conventions, drill teams, dance companies, studios, and more. She specializes in technique, 
+              contemporary, lyrical, jazz, tumbling, and is a certified Acro Dance teacher from Acrobatic Arts.
+              </h2>
             </div>
           </div>
           <div className='staff-column'>
             <div className='staff-pic'>
               <img src='/assets/staff-pics/Kellie+BWB.jpg'></img>
             </div>
-            <h1> +Kelli</h1>
+            <div className='header'>+ Kellie</div> 
             <div className='staff-bio'>
-            <h2>
-            Kellie won the title of Miss Dance/Drill Team Utah. She was then invited to Nagoya Japan where she won
-             the title of Miss Dance/Drill team International and won top honors with the Minister of Foreign Affairs
+              <h2>
+              Kellie won the title of Miss Dance/Drill Team Utah. She was then invited to Nagoya Japan where she won
+              the title of Miss Dance/Drill team International and won top honors with the Minister of Foreign Affairs
               Award. Kellie then continued her education at the University of Utah where she was the head Cheerleader. 
               Kellie danced professionally with the Odyssey Dance Utah for seven years. She was the director of
-               Dance Utah Summer Camp. She had the opportunity to direct Odysseys Junior Company, O2, for two years. 
-               Kellie’s real passion is teaching and choreographing. She has been teaching for over 20 years. 
-               She was nominated for the Who’s Who among Top Teachers. She has choreographed for Odyssey Dance 
-               Theatre, Miss Utah, dancers in the Star Search, dancers on So You Think You Can Dance, and Dancing
-                with the Stars. Kellie choreographed a music video “All for Nothing” for Mark Ballas. Kellie has 
-                choreographed Peter Pam, Footloose, Rag Time, and Wizard of Oz at the Scera Shell Outdoor 
-                Theatre. Kellie was a co-founder of The Vibe Performing Arts. In 2011 the Vibe was finalist on 
-                Paula Abduls dance show Live to Dance. She has choreographed and directed shows including Move,
-                 One Night Only, and Evolution of Dance. She took her dancers to Hong Kong China for Chinese New Year Parade.
-            </h2>
+              Dance Utah Summer Camp. She had the opportunity to direct Odysseys Junior Company, O2, for two years. 
+              Kellie’s real passion is teaching and choreographing. She has been teaching for over 20 years. 
+              She was nominated for the Who’s Who among Top Teachers. She has choreographed for Odyssey Dance 
+              Theatre, Miss Utah, dancers in the Star Search, dancers on So You Think You Can Dance, and Dancing
+              with the Stars. Kellie choreographed a music video “All for Nothing” for Mark Ballas. Kellie has 
+              choreographed Peter Pam, Footloose, Rag Time, and Wizard of Oz at the Scera Shell Outdoor 
+              Theatre. Kellie was a co-founder of The Vibe Performing Arts. In 2011 the Vibe was finalist on 
+              Paula Abduls dance show Live to Dance. She has choreographed and directed shows including Move,
+              One Night Only, and Evolution of Dance. She took her dancers to Hong Kong China for Chinese New Year Parade.
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className='staff-columns'>
+          <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Savannah+BWB.jpg'></img>
+            </div>
+            <div className='header'>+ Savannah</div> 
+            <div className='staff-bio'>
+              <h2>
+              Savannah grew up in Highland Utah and began her love of dance at the age of three. Throughout the years she began
+               to excel in her skills and received many dance scholarships at conventions nationwide; one of which was a full 
+               ride performing arts scholarship at the AMDA College and Conservatory of the Performing Arts, located in 
+               Los Angeles, as well as the American Musical and Dramatic Academy located in New York City. Savannah started 
+               teaching at a young age and found a passion for helping individuals find confidence in themselves, while
+                developing a love for hard work. She has choreographed many routines of all styles and loves to see those 
+                she is teaching to find success in their efforts. Savannah is upbeat, fun, and completely focused on 
+                individuals in order to help each member of the team to excel in the fastest way possible.
+              </h2>
+            </div>
+          </div>
+          <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Mandy+BWB.jpg'></img>
+            </div>
+            <div className='header'>+ Mandy </div> 
+            <div className='staff-bio'>
+              <h2>
+              Mandy has over 14 years of dance training and has spent the last several years focusing on choreography; 
+              choreographing over 150 routines locally and also as a visiting choreographer out of state. She is 
+              committed to helping students develop their own creativity as well as their technique. She has had
+               excellent instructors that have shown her how to be a teacher that inspires and motivates and 
+               that’s her ultimate goal. She continues to work on her own choreography and is continuing her
+                training as a student and teacher at Brigham Young University.
+              </h2>
+            </div>
+          </div>
+          <div className='staff-column'>
+            <div className='staff-pic'>
+              <img src='/assets/staff-pics/Alexis+BWB.jpg'></img>
+            </div>
+            <div className='header'> + Alexis </div> 
+            <div className='staff-bio'>
+              <h2>
+              Alexis has trained at The Dance Club since the age of 3 in ballet, jazz, hip hop, and contemporary. 
+              She has taught for over six years at the Vibe Performing Arts and three years at Bunker Dance Center
+              located in Las Vegas. She has trained and performed with the professional company Origins for
+              6 years. She has been on television shows such as “Live to Dance”, “Dancing with the Stars”, 
+              and “So You Think You Can Dance.” She has been featured in music videos and other various 
+              commercials. Encouraging kids to seek their full potential and lifting their self-esteem is
+              her specialty.
+              </h2>
             </div>
           </div>
         
+        </div>
       </div>
     </div>
 
